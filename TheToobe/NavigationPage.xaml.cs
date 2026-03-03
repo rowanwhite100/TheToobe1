afb8629aee4b847e2a2e1c5edb69e739d7163e0c
Rowan white <rowanwhite08@icloud.com>
2026-03-03 12:29:59 +0000

    updated and did nice things and general improvements

--- TheToobe/NavigationPage.xaml.cs
+++ TheToobe/NavigationPage.xaml.cs
@@ -1,4 +1,5 @@
 namespace TheToobe;
+
 using Microsoft.Data.Sqlite;
 using Microsoft.Maui;
 using Microsoft.Maui.ApplicationModel.DataTransfer;
@@ -9,10 +10,11 @@ using System;
 using System.Reflection.PortableExecutable;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
+using static SQLite.SQLite3;
 
 public partial class NavigationPage : ContentPage //inheritance
 {
-    public Dictionary<string, List<string>> list = BuildAdjacencyList();
+    //start of UI code
     public NavigationPage()
     {
         InitializeComponent();
@@ -20,46 +22,21 @@ public partial class NavigationPage : ContentPage //inheritance
     private void OnInitiateClicked(object sender, EventArgs args)
     {
 
-        string station1 = Regex.Replace(Station1_input.Text.ToLower(), @"\b([a-z])", m => m.Value.ToUpper());
-        string station2 = Regex.Replace(Station2_input.Text.ToLower(), @"\b([a-z])", m => m.Value.ToUpper()); //regex conversion to how the database is formatted
+        string origin = Regex.Replace(Station1_input.Text.ToLower(), @"\b([a-z])", m => m.Value.ToUpper());
+        string dstination = Regex.Replace(Station2_input.Text.ToLower(), @"\b([a-z])", m => m.Value.ToUpper()); //regex conversion to how the database is formatted
 
+        int Station1ID = CheckStation(origin);
+        int Station2ID = CheckStation(dstination);
 
-        //try //exeption handling
-        //{
-        //    // SELECT “StartStation”, “EndStation” FROM “StationConnections” WHERE “StartStation” = x OR “EndStation” = x
-        //    var sql = @"
-        //        SELECT Station1, Station2
-        //        FROM Connections
-        //        WHERE Station1 = @StationInput OR Station2 = @StationInput
-        //        ";
-        //    using var connection = new SqliteConnection("Data Source=\"C:\\Users\\rowan\\source\\repos\\NEA\\ToobeDataBase.db\""); //connecting to the database
-        //    connection.Open();
-        //    using var command = new SqliteCommand(sql, connection);
-        //    command.Parameters.AddWithValue("@StationInput", station1);
-
-        //    using var reader = command.ExecuteReader();
-
-        //    var outputLines = new System.Text.StringBuilder();
-        //    while (reader.Read())
-        //    {
-        //        var left = reader.IsDBNull(0) ? string.Empty : reader.GetString(0);
-        //        var right = reader.IsDBNull(1) ? string.Empty : reader.GetString(1);
-        //        outputLines.AppendLine($"{left} - {right}");
-        //    }
-
-        //    OutputBox.Text = outputLines.ToString().TrimEnd();
-
-
-
-        //    connection.Close();
-
-        //}
-        //catch (SqliteException e)
-        //{
-        //    // Display the exception
-        //    Console.WriteLine(e.Message);
-        //}
-
+        if (Station1ID == -1 || Station2ID == -1)
+        {
+            DisplayAlert("Error", "One or both of the stations entered were not found. Please check your spelling and try again.", "OK");
+        }
+        else
+        {
+            AstarTraversal(Station1ID, Station2ID);
+            //navigate to the next page and pass the station
+        }
     }
     private void OneTextChanged(object sender, TextChangedEventArgs e)
     {
@@ -83,7 +60,6 @@ public partial class NavigationPage : ContentPage //inheritance
             Initate_button.BackgroundColor = Color.FromArgb("#838D93");
         } 
     }
-
     private void TwoTextChanged(object sender, TextChangedEventArgs e)
     {
         string oldText = e.OldTextValue;
@@ -106,10 +82,56 @@ public partial class NavigationPage : ContentPage //inheritance
             Initate_button.BackgroundColor = Color.FromArgb("#838D93");
         }
     }
-    public static Dictionary<string, List<string>> BuildAdjacencyList()
+
+    //start of data proccessing 
+
+    public Dictionary<int, List<int>> graph = BuildAdjacencyList();
+
+
+    public int CheckStation(string Station)
     {
-        var adjacencyList = new Dictionary<string, List<string>>();
-        using var connection = new SqliteConnection("Data Source=\"C:\\Users\\rowan\\source\\repos\\NEA\\ToobeDataBase.db\""); //connecting to the database
+        try //exeption handling
+        {
+            var sql = @"
+                SELECT Name, StationID
+                FROM Stations
+                WHERE Name = @StationInput
+                ";
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "ToobeDataBase.db");
+            using var connection = new SqliteConnection(dbPath); //connecting to the database
+            connection.Open();
+            using var command = new SqliteCommand(sql, connection);
+            command.Parameters.AddWithValue("@StationInput", Station);
+
+            using var reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                string name = reader.GetString(0);
+                int id = reader.GetInt32(1);
+
+                if (name == Station)
+                {
+                    return id;
+                }
+                    
+            }
+
+            return -1; //station not found
+        }
+        catch (SqliteException e)
+        {
+            // Display the exception
+            Console.WriteLine(e.Message);
+        }
+
+        return -1; //station not found
+    }
+    public static Dictionary<int, List<int>> BuildAdjacencyList()
+    {
+        var adjacencyList = new Dictionary<int, List<int>>();
+        string dbPath = Path.Combine(FileSystem.AppDataDirectory, "ToobeDataBase.db");
+        using var connection = new SqliteConnection(dbPath); //connecting to the database
         try
         {
             connection.Open();
@@ -117,14 +139,14 @@ public partial class NavigationPage : ContentPage //inheritance
             var command = connection.CreateCommand();
             command.CommandText = @"
             SELECT Station1, Station2
-            FROM Connections;
-        ";
+            FROM Connections
+            ";
 
             using var reader = command.ExecuteReader();
             while (reader.Read())
             {
-                string start = reader.GetString(0);
-                string end = reader.GetString(1);
+                int start = reader.GetInt32(0);
+                int end = reader.GetInt32(1);
 
                 AddConnection(adjacencyList, start, end);
                 AddConnection(adjacencyList, end, start);
@@ -142,11 +164,11 @@ public partial class NavigationPage : ContentPage //inheritance
 
         return adjacencyList;
     }
-    private static void AddConnection(Dictionary<string, List<string>> graph, string start, string end)
+    private static void AddConnection(Dictionary<int, List<int>> graph, int start, int end)
     {
         if (!graph.TryGetValue(start, out var neighbors))
         {
-            neighbors = new List<string>();
+            neighbors = new List<int>();
             graph[start] = neighbors;
         }
 
@@ -155,55 +177,20 @@ public partial class NavigationPage : ContentPage //inheritance
             neighbors.Add(end);
         }//this avoids duplicate entries
     }
-    private void AstarTraversal(string Station1, string Station2)
+    private void AstarTraversal(int origin, int destination)
     {
-        int[] bestDistance = new int[303]; // array of best distances to each node from the source
-        PriorityQueue<string, int> nodesToVisit = new PriorityQueue<string, int>(); //priority queue of nodes to visit organised by heuristic + distance
-        int[] predecessors = new int[303]; //array of predecessors for each node
+        int[] bestDistance = new int[310]; // array of best distances to each node from the source
+        PriorityQueue<int, int> nodesToVisit = new PriorityQueue<int, int>(); //priority queue of nodes to visit organised by ID, heuristic + distance
+        int[] predecessors = new int[310]; //array of predecessors for each node
 
         for (int i = 0; i < bestDistance.Length; i++) //initialising best distances to infinity
         {
             bestDistance[i] = -1; //set all values to -1 to represent infinity as graph is unvisited
-            bestDistance[Convert.ToInt32(Station1)] = 0; //distance to source is 0
         }
+        bestDistance[Convert.ToInt32(origin)] = 0; //distance to source is 0
 
-        try //exeption handling
-        {
-            using var connection = new SqliteConnection("Data Source=\"C:\\Users\\rowan\\source\\repos\\NEA\\ToobeDataBase.db\""); //connecting to the database
-            connection.Open();
+        nodesToVisit.Enqueue(origin, 0); //enqueue origin with 0 distance
 
-            connection.Close();
-        }
-        catch (SqliteException e)
-        {
-            // Display the exception
-            Console.WriteLine(e.Message);
-        }
-
-        //take out all the connections and order by smallest staion number fisrst the remove any duplicaates
-        //this also allows for a true adgecency list to be created for easyer querying later
-    }
-
-
-
-}
-
-
-class Station
-{ 
-    private string Station_Name { get; set; }
-    private string Id { get; set; }
-    private float Latitude { get; set; }
-    private float Longitude { get; set; }
-    private float zone { get; set; }
-
-    public Station(string StationInput)
-    {
-
-        
 
     }
-
 }
-
-
